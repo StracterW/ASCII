541934cd838e7023fb28f6417e298f2543b94cdc
StracterW <164504803+StracterW@users.noreply.github.com>
2024-03-22 13:30:06 +0100

    Update ASCII_generator.py

--- ASCII_generator.py
+++ ASCII_generator.py
@@ -7,12 +7,11 @@ img = Image.open("image.jpg")
 convert_tensor = transforms.ToTensor()  # convert image to tensor
 tensor = convert_tensor(img)
 try:
-    light = (tensor[0]+tensor[1]+tensor[2])/3       # brightnes of the image 
+    light = (tensor[0]+tensor[1]+tensor[2])/3       # brightness of the image 
 except:
     light = tensor[0]
 file = open('symbols.txt', "w")
-# mit der Methode "w" wird die gesamte Datei überschrieben
-lsymb = ["##","QQ","HH","OO","II","ii","++","==","::","^^",".."]   # symbols for specific brightness
+lsymb = ["##","HH","OO","II","ii","==","::","..", ". "]   # Symbols for specific brightness
 file.write("")
 for i in light:
     help = ""
@@ -20,28 +19,22 @@ for i in light:
     for n in i:
         if float(n) > 0.95:
             symbol = lsymb[0]
-        elif float(n) > 0.9:
-            symbol = lsymb[1]
         elif float(n) > 0.8:
-            symbol = lsymb[2]
+            symbol = lsymb[1]
         elif float(n) > 0.7:
-            symbol = lsymb[3]
+            symbol = lsymb[2]
         elif float(n) > 0.6:
-            symbol = lsymb[4]
+            symbol = lsymb[3]
         elif float(n) > 0.5:
-            symbol = lsymb[5]
-        elif float(n) > 0.4:
-            symbol = lsymb[6]
+            symbol = lsymb[4]
         elif float(n) > 0.3:
-            symbol = lsymb[7]
+            symbol = lsymb[5]
         elif float(n) > 0.2:
-            symbol = lsymb[8]
-        elif float(n) > 0.1:
-            symbol = lsymb[9]
+            symbol = lsymb[6]
         elif float(n) > 0.01:
-            symbol = lsymb[10]
+            symbol = lsymb[7]
         else: 
-            symbol = ". "
+            symbol = lsymb[8]
         help +=symbol
     file.write("\n"+ str(help))
 file.close()
@@ -58,4 +51,4 @@ import cv2
 
 image = cv2.imread('output.png')
 invert = cv2.bitwise_not(image)
-cv2.imwrite('ZZpokeball.jpg', invert)             # switch white and black
+cv2.imwrite('result.jpg', invert)             # switch white and black
